515dc9b7b9962140b1a57b9366c7efe8868feae9
Carlos Mitchell <kakismitchell@gmail.com>
2020-02-13 01:09:21 -0500

    visual refrence in screen <h1>

--- api/server.js
+++ api/server.js
@@ -6,7 +6,8 @@ middleware(server);
 
 server.get("/", (req, res) => {
   console.log("It's alive!");
-  res.status(200).json({ message: "It's alive!" });
+  // res.status(200).json({ message: "It's alive!" });
+  res.send(`<h1> 🔨 Server is up and running</h1>`);
 });
 
 module.exports = server;
